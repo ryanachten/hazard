0a6625302e69bec1e25e2b29f36bd1dc3f0da9a3
Ryan Achten <ryanachten@gmail.com>
2026-06-30 09:33:40 +1200

    style: add lipgloss styles to characters

--- internal/common/utilities.go
+++ internal/common/utilities.go
@@ -9,6 +9,11 @@ func RandIntInRange(valueRange [2]int) int {
 	return low + rand.Intn(high-low+1)
 }
 
+// RandomFloat returns a random float within range
+func RandomFloat(low, high float64) float64 {
+	return low + rand.Float64()*(high-low)
+}
+
 // RandValInSlice returns a random element from a non-empty slice
 func RandValInSlice[T any](values []T) T {
 	randomIndex := rand.Intn(len(values))
